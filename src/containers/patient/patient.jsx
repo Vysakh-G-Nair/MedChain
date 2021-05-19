d10bf271e07708d7e1d445fedc6e094f8914f5c6
Salman A <salmana.ar@gmail.com>
2021-05-19 12:25:46 +0530

    Patient page fixed

--- src/containers/patient/patient.jsx
+++ src/containers/patient/patient.jsx
@@ -1,105 +1,120 @@
-import React from 'react';
-import './patientStyling.scss';
+import React from "react";
+import "./patientStyling.scss";
 import { Link, withRouter } from "react-router-dom";
-import view_your_record_img from './view_record.png';
-import grant_img from './grant.png';
-import view_external_img from './view external.png';
-import share_your_record_img from './share.png';
-import PatientCreator from '../../ethereum/patient';
+import view_your_record_img from "./view_record.png";
+import grant_img from "./grant.png";
+import view_external_img from "./view external.png";
+import share_your_record_img from "./share.png";
+import PatientCreator from "../../ethereum/patient";
 import web3 from "../../ethereum/web3";
-import { Details } from '../index.js' ;
+import { Details } from "../index.js";
 
 class Patient extends React.Component {
-    state = {
-        address: "",
-        addressOwner: '',
-        name: '',
-        age: '',
-        gender: '',
-        bloodGroup: '',
-        noOfRecords: -1
-    }
+  state = {
+    address: "",
+    addressOwner: "",
+    name: "",
+    age: "",
+    gender: "",
+    bloodGroup: "",
+    noOfRecords: -1,
+  };
 
-    componentWillMount() {
-        const { state } = this.props.location;
-        this.getPatSummary(state)
-    }
+  componentWillMount() {
+    const { state } = this.props.location;
+    this.getPatSummary(state);
+  }
 
-    async getPatSummary(state) {
-        const patient = PatientCreator(state);
-        console.log("Deployed address: " + patient.options.address);
+  async getPatSummary(state) {
+    const patient = PatientCreator(state);
+    console.log("Deployed address: " + patient.options.address);
 
-        const accounts = await web3.eth.getAccounts();
+    const accounts = await web3.eth.getAccounts();
 
-        const summary = await patient.methods.getPatSummary().call({
-            from: accounts[0]
-        });
+    const summary = await patient.methods.getPatSummary().call({
+      from: accounts[0],
+    });
 
-        this.setState({
-            address: state,
-            addressOwner: summary[0],
-            name : summary[1],
-            age: summary[2],
-            gender: summary[3],
-            bloodGroup: summary[4],
-            noOfRecords: summary[5]
-        });
-    }
+    this.setState({
+      address: state,
+      addressOwner: summary[0],
+      name: summary[1],
+      age: summary[2],
+      gender: summary[3],
+      bloodGroup: summary[4],
+      noOfRecords: summary[5],
+    });
+  }
 
-    render() {
-        const {
-            patient,
-            viewYourRecords,
-            name,
-            shareRecord,
-            text2,
-        } = this.props;
+  render() {
+    const { patient, viewYourRecords, name, shareRecord, text2 } = this.props;
 
-        const detailsData = {
-            overlapGroup: "https://anima-uploads.s3.amazonaws.com/projects/60891db35bdecf992a20f15c/releases/609cab0d2e5b4db0132e7a2a/img/rectangle-51@2x.svg",
-            spanText: "Name: ",
-            spanText2: <>{this.state.name}<br /></>,
-            spanText3: "Age: ",
-            spanText4: <>{this.state.age} years<br /></>,
-            spanText5: "Gender: ",
-            spanText6: <>{this.state.gender}<br /></>,
-            spanText7: "Blood group: ",
-            spanText8: <>{this.state.bloodGroup}<br /></>,
-            spanText9: "No of records: ",
-            spanText10: <>{this.state.noOfRecords}<br /></>,
-          };
-        
-        return (
-          <div class="container-center-horizontal">
-            <form
-              className="patient screen"
-              style={{ backgroundImage: `url(${patient})` }}
-              name="form4"
-              action="form4"
-              method="post"
-            >
-              <div className="details-comp">
-                {" "}
-                <Details {...detailsData} />{" "}
-              </div>
-              <div className="overlap-group-patient">
-                <div className="flex-row">
-                  <Link to="/viewrecords">
-                    <div className="group-57">
-                      <div className="overlap-group4-patient">
-                        <img
-                          className="view-4"
-                          src={view_your_record_img}
-                          alt=""
-                        />
-                        <div className="view-your-records poppins-medium-amethyst-16px">
-                          {viewYourRecords}
-                        </div>
-                      </div>
+    const detailsData = {
+      overlapGroup:
+        "https://anima-uploads.s3.amazonaws.com/projects/60891db35bdecf992a20f15c/releases/609cab0d2e5b4db0132e7a2a/img/rectangle-51@2x.svg",
+      spanText: "Name: ",
+      spanText2: (
+        <>
+          {this.state.name}
+          <br />
+        </>
+      ),
+      spanText3: "Age: ",
+      spanText4: (
+        <>
+          {this.state.age} years
+          <br />
+        </>
+      ),
+      spanText5: "Gender: ",
+      spanText6: (
+        <>
+          {this.state.gender}
+          <br />
+        </>
+      ),
+      spanText7: "Blood group: ",
+      spanText8: (
+        <>
+          {this.state.bloodGroup}
+          <br />
+        </>
+      ),
+      spanText9: "No of records: ",
+      spanText10: (
+        <>
+          {this.state.noOfRecords}
+          <br />
+        </>
+      ),
+    };
+
+    return (
+      <div class="container-center-horizontal">
+        <form
+          className="patient screen"
+          style={{ backgroundImage: `url(${patient})` }}
+          name="form4"
+          action="form4"
+          method="post"
+        >
+          <div className="details-comp">
+            {" "}
+            <Details {...detailsData} />{" "}
+          </div>
+          <div className="overlap-group-patient">
+            <div className="flex-row">
+              <Link to="/viewrecords">
+                <div className="group-57">
+                  <div className="overlap-group4-patient">
+                    <img className="view-4" src={view_your_record_img} alt="" />
+                    <div className="view-your-records poppins-medium-amethyst-16px">
+                      {viewYourRecords}
                     </div>
-
-                    </Link>
-                    {/* <Link to="/patientgrant">
+                  </div>
+                </div>
+              </Link>
+              {/* <Link to="/patientgrant">
                     <div className="group-patient">
                       <div className="overlap-group3-patient">
                         <img className="grant-1" src={grant_img} alt="" />
@@ -109,21 +124,21 @@ class Patient extends React.Component {
                       </div>
                     </div>
                     </Link> */}
-                    <Link to="/patientshare">
-                    <div className="group-patient">
-                      <div className="overlap-group2-patient">
-                        <img
-                          className="share-1"
-                          src={share_your_record_img}
-                          alt=""
-                        />
-                        <div className="share-record poppins-medium-amethyst-16px">
-                          {shareRecord}
-                        </div>
-                      </div>
+              <Link to="/patientshare">
+                <div className="group-patient">
+                  <div className="overlap-group2-patient">
+                    <img
+                      className="share-1"
+                      src={share_your_record_img}
+                      alt=""
+                    />
+                    <div className="share-record poppins-medium-amethyst-16px">
+                      {shareRecord}
                     </div>
-                    </Link>
-                    {/* <Link to="/patientexternalview">
+                  </div>
+                </div>
+              </Link>
+              {/* <Link to="/patientexternalview">
                     <div className="group-patient">
                       <div className="overlap-group1-patient">
                         <img
@@ -136,14 +151,14 @@ class Patient extends React.Component {
                         </div>
                       </div>
                     </div>
-                    </Link> */}
-                </div>
-                </div>
-              </div>
-            </form>
+                    </Link>
+                </div> */}
+            </div>
           </div>
-        );
-    }
+        </form>
+      </div>
+    );
+  }
 }
 
 export default withRouter(Patient);
