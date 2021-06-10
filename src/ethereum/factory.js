357eb2fd8223459125eaf01d723729a543ead1cd
Salman A <salmana.ar@gmail.com>
2021-06-10 20:14:07 +0530

    New deployment

--- src/ethereum/factory.js
+++ src/ethereum/factory.js
@@ -2,6 +2,6 @@ import web3 from './web3';
 import UserFactory from './build/UserFactory.json';
 
 const instance = new web3.eth.Contract(
-    JSON.parse(UserFactory.interface), '0x52a8b76d17350EC1962951ecE6A14a98a3b877D0');
+    JSON.parse(UserFactory.interface), '0xbBA374d2d5181463349BfC84af576e037598bB7C');
 
 export default instance;
